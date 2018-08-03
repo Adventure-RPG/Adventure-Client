99cf7e1b85d6782e3dd100256714dc017e0c27f4
Аллин Никита Евгеньевич <allinne@altarix.ru>
2018-08-03 16:28:54 +0300

    Пытаюсь исправить баг нового лоадера.

--- src/app/events/keyboard-event.service.ts
+++ src/app/events/keyboard-event.service.ts
@@ -84,16 +84,28 @@ export class KeyboardEventService {
         typeof this.storageService.hotkeySceneCommands[sceneCommandName].keyCode === 'number' &&
         <number>this.storageService.hotkeySceneCommands[sceneCommandName].keyCode === event.keyCode
       ) {
-        this.storageService.hotkeySceneCommands[sceneCommandName].onKeyDown(event);
-        this.storageService.hotkeySceneCommands[sceneCommandName].onKeyUp(event);
+
+        if (event.type === 'keyup'){
+          this.storageService.hotkeySceneCommands[sceneCommandName].onKeyUp(event);
+        }
+        if (event.type === 'keydown'){
+          this.storageService.hotkeySceneCommands[sceneCommandName].onKeyDown(event);
+        }
+
       } else if (
         typeof this.storageService.hotkeySceneCommands[sceneCommandName].keyCode === 'object' &&
         (<number[]>this.storageService.hotkeySceneCommands[sceneCommandName].keyCode).indexOf(
           event.keyCode
         ) !== -1
       ) {
-        this.storageService.hotkeySceneCommands[sceneCommandName].onKeyDown(event);
-        this.storageService.hotkeySceneCommands[sceneCommandName].onKeyUp(event);
+
+        if (event.type === 'keyup'){
+          this.storageService.hotkeySceneCommands[sceneCommandName].onKeyUp(event);
+        }
+        if (event.type === 'keydown'){
+          this.storageService.hotkeySceneCommands[sceneCommandName].onKeyDown(event);
+        }
+
       }
     }
   }
