382c8db4aff30ec74b60856b3aa31d11a440c1ba
Fwerf <afs1424ad@gmail.com>
2018-08-03 17:20:46 +0300

    ''

--- src/app/modules/engine/core/base/camera.service.ts
+++ src/app/modules/engine/core/base/camera.service.ts
@@ -88,6 +88,7 @@ export class CameraService implements OnInit {
     this.camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 20000);
     this.camera.position.set(d * 8, d * 8, d * 8);
     this.camera.rotation.y = (-135 * Math.PI) / 180;
+
     // require('three-first-person-controls')(THREE);
     //
     // console.log(FPC);
@@ -112,6 +113,7 @@ export class CameraService implements OnInit {
     let d = this.settingsService.settings.camera.d;
     this.camera = this.cameries[CAMERA.FirstPersonCamera];
     this.camera.position.set(d * 8, d * 8, d * 8);
+    (<PerspectiveCamera>this.camera).far = d * 40;
   }
 
   public initIsometricCamera() {
