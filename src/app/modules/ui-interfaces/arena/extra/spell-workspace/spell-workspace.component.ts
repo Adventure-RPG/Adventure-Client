7bbc7ed0a6ef4a3ab3267805dfbe4f19ef6d4757
Allin Nikita (Allen Wahlberg) <allinne@altarix.ru>
2019-07-17 02:59:33 +0300

    [+]Add window for effect tests

--- src/app/modules/ui-interfaces/arena/extra/spell-workspace/spell-workspace.component.ts
+++ src/app/modules/ui-interfaces/arena/extra/spell-workspace/spell-workspace.component.ts
@@ -1,4 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {Vector3} from "three";
+import {FormBuilder, Validators} from "@angular/forms";
+import {EnumHelpers} from "@enums/enum-helpers";
+
+//TODO: вынести spell
+export interface Spell {
+  friendlyFire: boolean,
+  range: number,
+  duration: number,
+  area: number,
+  type: SpellType,
+  savingThrow: SavingThrow,
+  spellResistance: boolean,
+  castingTime: Actions,
+
+  //LifeCycle for spells
+  cast: (vector: Vector3) => void,
+  update: (vector: Vector3) => void,
+  destroy: () => void,
+}
+
+export enum SpellType{
+  Single,
+  Line,
+  Cones,
+  Radius
+}
+
+export enum SavingThrow {
+  None,
+  Reflex,
+  Will,
+  Fortitude
+}
+
+export enum Actions {
+  FastAction,
+  MainAction,
+  FreeAction
+}
 
 @Component({
   selector: 'adventure-spell-workspace',
@@ -6,9 +46,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spell-workspace.component.scss']
 })
 export class SpellWorkspaceComponent implements OnInit {
-  selectedValue = 'single';
 
-  constructor() {}
+  spellForm = this.formBuilder.group({
+    friendlyFire: [false, [Validators.required]],
+    range: [10, [Validators.required]],
+    duration: [10, [Validators.required]],
+    area: [100, [Validators.required]],
+    type: [0, [Validators.required]],
+    savingThrow: [0, [Validators.required]],
+    spellResistance: [false, [Validators.required]],
+    castingTime: [0, [Validators.required]],
+  });
 
-  ngOnInit() {}
+
+  panel = {
+    active: true,
+    name: 'value',
+    disabled: false
+  };
+
+  options = {
+    spellType: this.enumValue(SpellType),
+    savingThrow: this.enumValue(SavingThrow),
+    actions: this.enumValue(Actions),
+  };
+
+
+  enumValue(enumValue){
+    return EnumHelpers.getSelectListAsArray(enumValue, (arg: number) => {
+      return enumValue[arg]
+    })
+  }
+
+  get spellFormValue() {
+    return this.spellForm.value as Spell;
+  }
+
+  submitForm(){
+    //TODO: отсюда вызывать сервис
+    console.log(this.spellFormValue);
+    for (const i in this.spellForm.controls) {
+      this.spellForm.controls[i].markAsDirty();
+      this.spellForm.controls[i].updateValueAndValidity();
+    }
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+  }
+
+  ngOnInit() {
+  }
 }
