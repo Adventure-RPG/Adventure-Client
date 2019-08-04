188a5b7408eb17a24745e3d8d5c5a856b2b951b4
Allin Nikita (Allen Wahlberg) <allinne@altarix.ru>
2019-08-05 00:15:08 +0300

    [+] Изменил подключение нг зоро модуля

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,6 @@ const Modules = [
   HttpClientModule,
   UiInterfacesModule,
   UiInterfacesRoutingModule,
-  NgZorroAntdModule,
 ];
 
 const ModulesForRoot = [];
--- src/app/shared/shared-module.module.ts
+++ src/app/shared/shared-module.module.ts
@@ -9,6 +9,7 @@ import { SceneEventsDirective } from './directives/scene-events.directive';
 import { CheckboxComponent } from './form-components/checkbox/checkbox.component';
 import { RangeComponent } from './form-components/range/range.component';
 import { SanitizeHtmlDirective } from '@shared/directives/sanitize-html.directive';
+import { NgZorroAntdModule } from "ng-zorro-antd";
 
 const Modules = [
   CommonModule,
@@ -17,6 +18,7 @@ const Modules = [
   ReactiveFormsModule,
   ColorPickerModule,
   SnotifyModule,
+  NgZorroAntdModule
 ];
 
 const Services = [];
